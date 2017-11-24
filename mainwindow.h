f4bd21a139ab93acb8e4d911c36146da345ea86d
Thomas <thomas@echoes.fr>
2017-11-24 18:03:20 +0100

    adding function to startstop button

--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,14 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_buttonStartStop_pressed();
+
 private:
     Ui::MainWindow *ui;
+    bool started;
+    void start();
+    void stop();
 };
 
 #endif // MAINWINDOW_H
